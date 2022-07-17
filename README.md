1794aa4ff8784df0b5bde4b60664b648142f1c12
davidbesky <38742461+dmbesky@users.noreply.github.com>
2022-07-16 20:49:31 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# svm_tag_smoother
- A pitch type tag cleaner using SVM models
+# SVM Pitch Type Tag Smoother
+
+## Intro
+
+With pitch tracking technology, pitch classifications can be inconsistent depending upon how they're generated. 
+Assuming these tags are broadly correct just inconsistent, we may be able to generate more consistent tags by fitting a rigid SVM for each pitcher to find the dividing lines between the pitch types in a feature space including the critical pitch movement metrics (velocity, horizontal break, vertical break).
+
+*svm_pitch_tag_cleaner.R* takes in a csv with the required column types (*sample_data.csv* illustrates the required csv specifications) and returns a copy of said dataframe with the SVM fit pitch type tags as well. It also includes a function - visualize_tag_adjustments() - for comparing the initial pitch type tags with the SVM based tags.
+
+This approach is not necessarily the most rigorous (and some further hyperparameter tuning may be in order), but I thought was an efficient way to leverage the information from the existing tags rather than ignoring those and attempting to reclassify pitches from scratch - when I was poking around with bulk cleaning pitch tags accounting for different pitchers arsenals.
+
+That said, the results here depend upon the initial pitch tags being generally directionally correct - and may output weird results when that does not hold. Furthermore, pitch types with only a couple occurences prove difficult to handle.
+
+Nonetheless, I believe this approach may provide some value relative to applying global classification techniques and/or in it's simplicity. A couple of examples showing how it adjusts tags:
+
+![svm_pitch_tag_example_1](https://user-images.githubusercontent.com/38742461/179382989-823f3186-64e0-4b68-981b-495d0fd69898.jpg)
+
+![svm_pitch_tag_example_2](https://user-images.githubusercontent.com/38742461/179383005-5136cbe3-474a-499c-8fd7-0a8e689d5330.jpg)
+
+## Dependencies
+
+The following packages are required:
+
+* e1071
+* magrittr
+* dplyr
+* ggplot2
+* cowplot
---
+++ svm_pitch_tag_cleaner.R
@@ -0,0 +1,121 @@
+library(e1071) #for svm function
+library(magrittr) #for %>%
+library(dplyr) #for summarize and filter
+library(ggplot2) #for the pitch label comparison visuals
+library(cowplot) #for plot_grid
+
+init_data = read.csv('sample_data.csv') #required column names are pitcher_id, initial_pitch_type, velo, vert_break, horz_break
+
+init_data = init_data %>% 
+  filter_at(vars(velo,vert_break, horz_break),any_vars(!is.na(.))) %>%
+  mutate(initial_pitch_type = as.factor(initial_pitch_type))
+
+#Function to Createa a Separate SVM Model for each pitcher - to customize pitch tags to their unique clusters and pitch types
+tag_cleaner <- function(spec_pitcher_id){
+  print(spec_pitcher_id)
+  #Filter to just specific pitcher and remove any pitches with NAs in Velo, HB, VB 
+  pdata <- init_data %>% filter(pitcher_id == spec_pitcher_id)  
+
+  #Create the training dataset - just the tagged pitches and only the specific columns
+  predvars <-pdata %>% filter(!is.na(initial_pitch_type))
+  
+  #Fit the SVM and then predict the new tags - parameters manually set through trial and error to achieve the desired effect
+  svmfit = svm(initial_pitch_type ~ velo + vert_break + horz_break, data = predvars, kernel = "linear", cost = 0.5, scale = TRUE)
+  pdata$svm_pitch_type <- predict(svmfit, pdata)
+  
+  return(pdata)
+}  
+
+#Create a Grid of ggplot's illustrating the initial and cleaned pitch tags
+visualize_tag_adjustments <- function(spec_pitcher_id){
+  spec_pitcher_data = output %>% filter(pitcher_id == spec_pitcher_id)
+  
+  #define a consistent color scale for pitch tags - across the different plots here - with only the relevant pitch types in the legend
+  colors_for_scale = c('#0072b2', "#ffa300", '#009e73', '#F0E442', '#CC79A7', '#9d79cc', '#3dad26', "#fc3535", 'gray50', '#000000')
+  names(colors_for_scale) <- unique(spec_pitcher_data$initial_pitch_type)
+  pitch_type_color_scale <- scale_colour_manual(name = "Pitch Type", values = colors_for_scale)
+  
+  plot1 = spec_pitcher_data %>% ggplot(aes(x = horz_break, y= vert_break)) + geom_point(aes(color = initial_pitch_type)) + 
+    ggtitle('Initial Tags', 'Movement Plot') + pitch_type_color_scale + theme(legend.position="none")
+  plot2 = spec_pitcher_data %>% ggplot(aes(x = velo, y= vert_break)) + geom_point(aes(color = initial_pitch_type)) + 
+    ggtitle('Initial Tags', 'Velo and VB') + pitch_type_color_scale + theme(legend.position="none")
+
+  plot3 = spec_pitcher_data %>% ggplot(aes(x = horz_break, y= vert_break)) + geom_point(aes(color = svm_pitch_type))  + 
+    ggtitle('Cleaned Tags', 'Movement Plots') + pitch_type_color_scale + theme(legend.position="none")
+  plot4 = spec_pitcher_data %>% ggplot(aes(x = velo, y= vert_break)) + geom_point(aes(color = svm_pitch_type)) + 
+    ggtitle('Cleaned Tags', 'Velo and VB') + pitch_type_color_scale + theme(legend.position="none")
+  
+  #Add the shared legend and create the grid of plots
+  legend <- get_legend(plot1 + theme(legend.position = 'bottom', legend.spacing.x = unit(0.75, 'cm')))
+  scatter_plots = plot_grid(plot1, plot2, plot3, plot4)
+  scatter_plots = plot_grid(scatter_plots, legend, ncol = 1, rel_heights = c(8, 1))
+  
+  return(scatter_plots)
+}
+
+
+#Require at least 20 pitches of 2 pitch types, so the model has something reasonable to work with
+qualifiers <- init_data %>% filter(!is.na(initial_pitch_type)) %>% group_by(pitcher_id) %>% 
+  summarize(numtaggedpitches = n(), numpitches = n_distinct(initial_pitch_type)) %>% 
+  filter(numtaggedpitches >= 20, numpitches >= 2)
+qualifiers <- unique(qualifiers$pitcher_id)
+
+
+output= bind_rows(lapply(qualifiers, tag_cleaner)) #apply function for each pitcher than merge to one dataframe
+
+visualize_tag_adjustments('Test Athlete 1') #plot the adjustment for a specific athlete
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Load the Data from DB for Illustration Purposes
+library(RMySQL)
+
+statcast_db <- dbConnect(MySQL(), user=Sys.getenv('CLUSTER_USERNAME_DB_BIOMECH'),
+                         password=Sys.getenv('CLUSTER_PASSWORD_DB_BIOMECH'), 
+                         dbname='statcast_db',
+                         port=as.numeric(Sys.getenv('CLUSTER_PORT_DB_BIOMECH')),
+                         host=Sys.getenv('CLUSTER_HOST_DB_BIOMECH'))
+
+init_data = dbGetQuery(statcast_db, 'SELECT Pitcher as pitcher_id, TaggedPitchType as initial_pitch_type, RelSpeed as velo, InducedVertBreak as vert_break, HorzBreak as horz_break  FROM college_trackman')
+init_data <- init_data  %>%
+  mutate(initial_pitch_type = as.factor(initial_pitch_type))  #ensure pitch type is a factor
+
+
+
+
+
+
+
+test_data = init_data %>% filter(pitcher_id %in% c('Parsons, Hunter', 'Hall, Charles'))
+
+test_data$pitcher_id[test_data$pitcher_id == 'Parsons, Hunter'] <- 'Test Athlete 1'
+test_data$pitcher_id[test_data$pitcher_id == 'Hall, Charles'] <- 'Test Athlete 2'
+
+test_data$velo = test_data$velo + rnorm(nrow(test_data), mean = 0, sd = 0.2)
+test_data$horz_break = test_data$horz_break + rnorm(nrow(test_data), mean = 0, sd = 0.2)
+test_data$vert_break = test_data$vert_break + rnorm(nrow(test_data), mean = 0, sd = 0.2)
+
+test_data$velo = round(test_data$velo, 1)
+test_data$horz_break = round(test_data$horz_break, 1)
+test_data$vert_break = round(test_data$vert_break, 1)
+
+getwd()
+
+write_csv(test_data, 'sample_data.csv')
+
+
+
